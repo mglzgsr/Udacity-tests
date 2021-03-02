8328ce5297af25f5d82ea789680401abdaa42eaa
Miguel Izaga <miguel.izaga@rackspace.co.uk>
2021-03-02 11:35:40 +0000

    new files

---
+++ scripts/create_sf.sh
@@ -0,0 +1,6 @@
+aws cloudformation create-stack \
+--stack-name $1 \
+--template-body file://$2  \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+--region=us-west-2 \
+--profile personal
