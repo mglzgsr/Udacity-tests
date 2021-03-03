2f2088456a6296a29501c55b8b17b528fc5317ea
Miguel Izaga <miguel.izaga@rackspace.co.uk>
2021-03-03 20:31:46 +0000

    created files needed for next step

---
+++ scripts/describe.sh
@@ -0,0 +1,6 @@
+echo [all] > ../inventory.txt
+aws ec2 describe-instances \
+ --query 'Reservations[*].Instances[*].PublicIpAddress' \
+ --region=us-west-2 \
+ --profile=personal \
+ --output text >> ../inventory.txt
