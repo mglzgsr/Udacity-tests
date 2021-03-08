6a38a920d29bbff308e772e19f3c2f48ce15397f
Miguel Izaga <miguel.izaga@rackspace.co.uk>
2021-03-08 10:15:55 +0000

    testing with Lars

--- scripts/describe.sh
+++ scripts/describe.sh
@@ -2,5 +2,4 @@ echo [all] > inventory.txt
 aws ec2 describe-instances \
  --query 'Reservations[*].Instances[*].PublicIpAddress' \
  --region=us-west-2 \
- --profile personal \
  --output text >> inventory.txt
